0a9ed0602ef1d71e86eaaa9ce2be6fcc40f366bd
A. Diamond <diamond@sheprador.com>
2015-01-16 17:21:42 -0500

    Added missing items to Vagrant setup

---
+++ roles/go/files/install_nsq.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Install the NSQ binaries, if they are not already installed.
+#
+
+if [ ! -e ~/go/bin/nsqd ]; then
+    curl "https://s3.amazonaws.com/bitly-downloads/nsq/nsq-0.3.0.linux-amd64.go1.3.3.tar.gz" > nsq-0.3.0.linux-amd64.go1.3.3.tar.gz
+    tar -xzf nsq-0.3.0.linux-amd64.go1.3.3.tar.gz
+    cd nsq-0.3.0.linux-amd64.go1.3.3/bin
+    cp * ~/go/bin
+    rm nsq-0.3.0.linux-amd64.go1.3.3.tar.gz
+    rm -rf nsq-0.3.0.linux-amd64.go1.3.3
+fi

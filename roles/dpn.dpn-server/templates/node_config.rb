7a08c8e6b75702caf29fa62f51cbdfb69eac2869
Christian Dahlhausen <dahlhausen@gmail.com>
2016-12-12 12:22:03 -0500

    Parse nodes dict as list

--- roles/dpn.dpn-server/templates/node_config.rb
+++
@@ -1,8 +0,0 @@
-{% for node in nodeitem[item] %}
-Node.create!(
-  name: "{{ node.name}}", # Note this need *not* be the canonical name
-  namespace: "{{node.namespace}}",
-  api_root: "{{node.api_root}}",
-  private_auth_token: "{{node.token}}",
-  auth_credential: "{{node._credential}}")
-{% endfor %}

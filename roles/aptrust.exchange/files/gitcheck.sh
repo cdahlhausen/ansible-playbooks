e5749f85f1606332e324b85dfc28aabcd7e0911e
Christian Dahlhausen <dahlhausen@gmail.com>
2017-01-10 16:07:24 -0500

    Add templates.

--- roles/aptrust.exchange/files/gitcheck.sh
+++ roles/aptrust.exchange/files/gitcheck.sh
@@ -1,4 +1,22 @@
 #/bin/bash
-cd $1
-[ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
-  sed 's/\// /g') | cut -f1) ] && echo up to date || git reset --HARD
+# This checks if the code on github was updated and runs a git reset since the
+# config for the environment has been changed in the source directory which
+# prevents a go -u -f -d later.
+
+#cd $1
+#[ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
+#  sed 's/\// /g') | cut -f1) ] && echo up to date || git reset --HARD
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+elif [ $REMOTE = $BASE ]; then
+    echo "Need to push"
+else
+    echo "Diverged"
+fi
